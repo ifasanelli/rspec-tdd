8da1bdcd61e7e1ed22ed9bef68f2739968b34e8c
ifasanelli <fasanelli.italo@gmail.com>
2020-07-05 14:06:04 -0300

    matchers - matcher comparison

--- exemplo_rspec_tdd/spec/matchers/comparacao/comparacao_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/comparacao/comparacao_spec.rb
@@ -1,19 +1,43 @@
 describe 'Matchers de Comparação' do
-  it '#equal - testa se é o mesmo objeto' do
-    x = 'ruby'
-    y = 'ruby'
-    expect(x).to_not equal(y)
+  it '>' do
+    expect(5).to be > 1
   end
 
-  it '#be - testa se é o mesmo objeto' do
-    x = 'ruby'
-    y = 'ruby'
-    expect(x).to_not be(y)
+  it '>=' do
+    expect(5).to be >= 5
+    expect(5).to be >= 2
   end
 
-  it '#eql - testa o valor' do
-    x = 'ruby'
-    y = 'ruby'
-    expect(x).to eql(y)
+  it '<' do
+    expect(5).to be < 10
+  end
+
+  it '<=' do
+    expect(5).to be <= 5
+    expect(5).to be <= 8
+  end
+
+  it 'be_between' do
+    expect(2).to be_between(2,7).inclusive
+    expect(5).to be_between(2,7).inclusive
+    expect(7).to be_between(2,7).inclusive
+  end
+
+  it 'be_between' do
+    expect(5).to be_between(2,7).exclusive
+  end
+
+  it 'match' do
+    expect('fulado@email.com').to match(/..@../)
+  end
+
+  it 'start_with' do
+    expect('fulado de tal').to start_with('fulado')
+    expect([1,2,3]).to start_with(1)
+  end
+
+  it 'end_with' do
+    expect('fulado de tal').to end_with('tal')
+    expect([1,2,3]).to end_with(3)
   end
 end
