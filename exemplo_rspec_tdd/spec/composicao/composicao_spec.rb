4749bb5bb71e991bac1937cbe429571143bb49c1
ifasanelli <fasanelli.italo@gmail.com>
2020-07-10 00:58:12 -0300

    spec helpers

--- exemplo_rspec_tdd/spec/composicao/composicao_spec.rb
+++ exemplo_rspec_tdd/spec/composicao/composicao_spec.rb
@@ -1,9 +1,16 @@
+require_relative '../helpers/helper'
+
+RSpec.configure do |conf|
+  conf.include Helper
+end
+
 describe 'Ruby on Rails' do
   it { is_expected.to start_with('Ruby').and end_with('Rails') }
 
   it { expect(fruta).to eq('banana').or eq('laranja').or eq('uva') }
 
-  def fruta
-    %w(banana laranja uva).sample
-  end
+  # helper method arbitrário
+  # def fruta
+  #   %w(banana laranja uva).sample
+  # end
 end
