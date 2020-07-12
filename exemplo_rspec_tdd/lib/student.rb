df0b49db001d5cad6e1dd32a6cbe4cfb310a0c50
ifasanelli <fasanelli.italo@gmail.com>
2020-07-11 23:42:14 -0300

    stub methods

--- exemplo_rspec_tdd/lib/student.rb
+++ exemplo_rspec_tdd/lib/student.rb
@@ -4,4 +4,12 @@ class Student
   def has_finished?(course)
     # retornaria true ou false
   end
+
+  def foo(args)
+    # retornaria alguma coisa
+  end
+
+  def bar
+    # retornaria alguma coisa
+  end
 end
--- exemplo_rspec_tdd/spec/stubs/stubs_spec.rb
+++ exemplo_rspec_tdd/spec/stubs/stubs_spec.rb
@@ -15,4 +15,41 @@ describe 'Stub' do
 
     expect(course_finished).to be_truthy
   end
+
+  it 'Argumentos Dinâmicos' do
+    student = Student.new
+
+    # stub
+    allow(student).to receive(:foo) do |arg|
+      if arg == :hello
+        'Olá'
+      elsif arg == :hi
+        'Hi!!!'
+      end
+    end
+
+    expect(student.foo(:hello)).to eq('Olá')
+    expect(student.foo(:hi)).to eq('Hi!!!')
+  end
+
+  it 'Qualquer instância de class' do
+    student = Student.new
+    other_student = Student.new
+
+    # stub
+    allow_any_instance_of(Student).to receive(:bar)
+                                  .and_return(true)
+
+    expect(student.bar).to be_truthy
+    expect(other_student.bar).to be_truthy
+  end
+
+  it 'Erros' do
+    student = Student.new
+
+    # stub
+    allow(student).to receive(:bar).and_raise(RuntimeError)
+
+    expect{ student.bar }.to raise_error(RuntimeError)
+  end
 end
